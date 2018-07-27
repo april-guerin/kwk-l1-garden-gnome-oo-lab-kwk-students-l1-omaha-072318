e814df79a2ad32bb0ef0d6d263cf9ed7e72e005b
april-guerin <aprilguerin@yahoo.com>
2018-07-27 18:48:30 +0000

    automatically backed up by learn

--- garden_gnome.rb
+++ garden_gnome.rb
@@ -1,1 +1,55 @@
 # Code your instances here
+class GardenGnome
+  def initializes()
+    @personality = "evil"
+    @hat_color = hat_color
+  end
+  def personality
+    @personality = "evil"
+  end
+  def hat_color
+    @hat_color
+  end
+  
+  def name=(name)
+    @name = name
+  end
+  def name
+    @name
+  end
+  
+  def age=(age)
+    @age = age
+  end
+  def age
+    @age
+  end
+  
+  def gluten_allergy=(gluten_allergy)
+    @gluten_allergy = gluten_allergy
+  end
+  def gluten_allergy
+    @gluten_allergy
+  end
+  
+  def gnaw=(gnaw)
+    @gnaw = gnaw
+  end
+  def gnaw
+    "Gnawing on a tree!!!"
+  end
+  
+  def shout=(shout)
+    @shout = shout
+  end
+  def shout
+    "GNARLY!!!"
+  end
+  
+  def introduce_self=(introduce_self)
+    @introduce_self = introduce_self
+  end
+  def introduce_self
+    "Hello humans, my name is #{name}, I am #{age} years old, and you'll rue the day you crossed me!"
+  end
+end
